d289b45aa24d8b51ef9c0874b2c0b12820edce31
MohdWaqar98 <mohdwaqar695@gmail.com>
2025-05-13 13:19:16 +0530

    project switch

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -15,7 +15,7 @@ interface ProjectLink {
 export function Footer() {
   const socialLinks = [
     {
-      href: "https://github.com/MohdWaqar98",
+      href: "https://github.com/Aditi-57",
       icon: <LucideGithub className="w-5 h-5 text-zinc-500 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors" />,
     },
     {
